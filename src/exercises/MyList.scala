a0e63c8b720be6b2a541db6c87c883bc64a8c7ce
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-07-15 14:34:14 +0300

    Add solutions to object-oriented exercises

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -7,6 +7,10 @@ abstract class MyList[+A] {
   def add[B >: A](a: B): MyList[B]
   def print: String
   override def toString: String = "[" + print + "]"
+  def map[B](transformer: MyTransformer[A, B]): MyList[B]
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
+  def filter(predicate: MyPredicate[A]): MyList[A]
+  def ++[B >: A](list: MyList[B]): MyList[B]
 }
 
 object Empty extends MyList[Nothing] {
@@ -15,6 +19,10 @@ object Empty extends MyList[Nothing] {
   def isEmpty: Boolean = true
   def add[B >: Nothing](a: B): MyList[B] = new MyListImpl(a, Empty)
   def print: String = ""
+  def map[B](transformer: MyTransformer[Nothing, B]): MyList[B] = Empty
+  def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = Empty
+  def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
+  def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 }
 
 class MyListImpl[+A](h: A, t: MyList[A]) extends MyList[A] {
@@ -25,6 +33,22 @@ class MyListImpl[+A](h: A, t: MyList[A]) extends MyList[A] {
   def print: String =
     if (tail.isEmpty) "" + h
     else h + " " + t.print
+  def map[B](transformer: MyTransformer[A, B]): MyList[B] =
+    new MyListImpl(transformer.transform(h), t.map(transformer))
+  def ++[B >: A](list: MyList[B]): MyList[B] = new MyListImpl(h, t ++ list)
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] =
+    transformer.transform(h) ++ t.flatMap(transformer)
+  def filter(predicate: MyPredicate[A]): MyList[A] =
+    if (predicate.test(h)) new MyListImpl(h, t.filter(predicate))
+    else t.filter(predicate)
+}
+
+trait MyPredicate[-T] {
+  def test(el: T): Boolean
+}
+
+trait MyTransformer[-A, B] {
+  def transform(el: A): B
 }
 
 object MyListTest extends App {
@@ -34,6 +58,14 @@ object MyListTest extends App {
   println(listOfIntegers.toString)
   println(listOfStrings.toString)
   println(listOfIntegers.head)
+
+  println(listOfIntegers.map(new MyTransformer[Int, Int] {
+    override def transform(el: Int): Int = el * 2
+  }))
+
+  println(listOfIntegers.filter(new MyPredicate[Int] {
+    override def test(el: Int): Boolean = el % 2 == 0
+  }).toString)
 
 
 }
