47267509e0236c0a2e546c19b9768dc8a4ef617b
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-07-15 10:17:01 +0300

    Make MyList generic

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -1,35 +1,39 @@
 package exercises
 
-abstract class MyList {
-  def head: Int
-  def tail: MyList
+abstract class MyList[+A] {
+  def head: A
+  def tail: MyList[A]
   def isEmpty: Boolean
-  def add(a: Int): MyList
+  def add[B >: A](a: B): MyList[B]
   def print: String
   override def toString: String = "[" + print + "]"
 }
 
-object Empty extends MyList {
-  def head: Int = throw new NoSuchElementException
-  def tail: MyList = throw new NoSuchElementException
+object Empty extends MyList[Nothing] {
+  def head: Nothing = throw new NoSuchElementException
+  def tail: Nothing = throw new NoSuchElementException
   def isEmpty: Boolean = true
-  def add(a: Int): MyList = new MyListImpl(a, Empty)
+  def add[B >: Nothing](a: B): MyList[B] = new MyListImpl(a, Empty)
   def print: String = ""
 }
 
-class MyListImpl(h: Int, t: MyList) extends MyList {
-  def head: Int = h
-  def tail: MyList = t
+class MyListImpl[+A](h: A, t: MyList[A]) extends MyList[A] {
+  def head: A = h
+  def tail: MyList[A] = t
   def isEmpty: Boolean = false
-  def add(a: Int): MyList = new MyListImpl(a, this)
+  def add[B >: A](a: B): MyList[B] = new MyListImpl(a, this)
   def print: String =
     if (tail.isEmpty) "" + h
     else h + " " + t.print
 }
 
 object MyListTest extends App {
-  val list = new MyListImpl(1, new MyListImpl(2, new MyListImpl(4, Empty)))
-  println(list.head)
-  println(list.tail)
-  println(list.toString)
+  val listOfIntegers: MyList[Int] = new MyListImpl(1, new MyListImpl(2, Empty))
+  val listOfStrings: MyList[String] = new MyListImpl("Java", new MyListImpl("Scala", Empty))
+
+  println(listOfIntegers.toString)
+  println(listOfStrings.toString)
+  println(listOfIntegers.head)
+
+
 }
