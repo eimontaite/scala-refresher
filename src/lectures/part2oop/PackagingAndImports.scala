ed18e719f82c20d166b666924092d02c003007a5
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-07-15 18:30:56 +0300

    Add material for Lecture 18, packaging and imports

---
+++ src/lectures/part2oop/PackagingAndImports.scala
@@ -0,0 +1,9 @@
+package lectures.part2oop
+
+class PackagingAndImports {
+
+  // package members are accessible by their simple name if they're in the same package
+  // otherwise they need to be imported or their fully qualified class name should be used
+  // packages are in a hierarchy that matches the file system
+  sayHello
+}
---
+++ src/lectures/part2oop/package.scala
@@ -0,0 +1,7 @@
+package lectures
+
+// package object - visible throughout the entire object
+package object part2oop {
+  def sayHello = println("Hello!")
+
+}
