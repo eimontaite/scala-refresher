f045697bc8545b31fbdabb31da3661998d2cf4a5
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-07-16 17:33:47 +0300

    Add material for Lecture 19, functions

---
+++ src/lectures/part3fp/WhatIsAFunction.scala
@@ -0,0 +1,28 @@
+package lectures.part3fp
+
+object WhatIsAFunction extends App {
+
+  val doubler = new MyFunction[Int, Int] {
+    override def apply(element: Int): Int = element * 2
+  }
+
+  println(doubler(2))
+
+  // function types = Function1...22[A, B]
+  val stringToIntConverter = new Function1[String, Int] {
+    override def apply(string: String): Int = string.toInt
+  }
+
+  println(stringToIntConverter("3") + 4)
+
+  val adder: (Int, Int) => Int = new Function2[Int, Int, Int] {
+    override def apply(a: Int, b: Int): Int = a + b
+  }
+
+  // all Scala functions are objects
+}
+
+
+trait MyFunction[A, B] {
+  def apply(element: A): B
+}
